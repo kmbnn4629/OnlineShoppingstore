f39feb92e16e429cb66fe44fd0771f2d5ddad4f9
ARIT <ARIT@ARIT-51010017.arit.co.th>
2017-07-18 10:43:38 +0700

    Testtt Commit

---
+++ src/javaapplication5/Item.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication5;
+
+/**
+ *
+ * @author ARIT
+ */
+public class Item {
+    private int id;
+    private String name;
+    private double price;
+    
+    public Item
+    
+}
